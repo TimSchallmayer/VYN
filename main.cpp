1e892ac5191d0c20764d34fd52d7b5ae6cf492e5
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2026-01-27 19:33:12 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << std::endl;
-    FILE *datei = fopen("test.txt", "r");
+    fstream datei ("main.exe", ios::binary | ios::in);
 
+    if (datei.bad()) {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+    streampos begin, end;
+    begin = datei.tellg();
+    datei.seekg(0, ios::end);
+    end = datei.tellg();
+    datei.close();
+    cout << "Size in bytes: " << (end - begin) << endl;
     return 0;
 }
