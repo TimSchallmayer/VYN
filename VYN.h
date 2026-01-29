4a78e055d9e2a3860e0e9281fbf3106de7402a36
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2026-01-29 21:08:35 +0100

    added useful comments to the function prototypes

--- VYN.h
+++ VYN.h
@@ -4,6 +4,13 @@
 
 // Prototypen der Funktionen:
 SDL_Window* init_SDL(int w, int h);
+// SDL starten
+
 SDL_Renderer* init_renderer(SDL_Window *window);
+// SDL renderer starten
+
 bool quit(SDL_Renderer *renderer, SDL_Window *window);
+// SDL schließen und alle instancen beenden
+
 void draw(SDL_Renderer *renderer);
+// malt ein rotes rechteck an die aktuelle maus position
