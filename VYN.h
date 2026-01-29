708deca93f53df1f96123fe5be500ca2d3720106
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2026-01-29 21:01:22 +0100

    added the draw function  for red rect game

--- VYN.h
+++ VYN.h
@@ -5,3 +5,5 @@
 // Prototypen der Funktionen:
 SDL_Window* init_SDL(int w, int h);
 SDL_Renderer* init_renderer(SDL_Window *window);
+bool quit(SDL_Renderer *renderer, SDL_Window *window);
+void draw(SDL_Renderer *renderer);
